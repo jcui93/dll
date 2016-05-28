257e24b8d4ff35c96eb9271666148b9a8a29ecc0
tallstout00 <cahburns@gmail.com>
2016-05-28 11:49:38 -0700

    initial commit

---
+++ import_test/MainWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton1Clicked (object sender, EventArgs e)
+	{
+		import_test.importer tester = new import_test.importer();
+	}
+}
---
+++ import_test/importer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace import_test
+{
+	public class importer
+	{
+		[DllImport ("/home/clint/import_test/vehicle-scan.so",
+		CallingConvention = CallingConvention.StdCall )] 
+		unsafe static extern void cmd_lescan (int dev_id, int argc, char**argv);
+
+		unsafe public importer ()
+		{
+			char** tester = null;
+			try{
+			cmd_lescan(-1, 1, tester);
+			}
+			catch(DllNotFoundException e){
+				Console.WriteLine (e.ToString ());
+			}
+			catch(EntryPointNotFoundException e){
+				Console.WriteLine (e.ToString ());
+			}
+		}
+	}
+}
+
